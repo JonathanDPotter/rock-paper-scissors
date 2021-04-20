c5af5fe6cc7570f2b2e6e24a48a6b27d57f531bb
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-04-20 14:27:31 -0400

    Updated with GUI

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
+let ps = 0;
+let cs = 0;
+let result = document.createElement("p");
+let playerScore = document.createElement("h1");
+let computerScore = document.createElement("h1");
+
 function computerPlay() {
   let compChoice = Math.floor(Math.random() * 3);
   return checkChoice(compChoice);
-}
-
-function getSelection() {
-  let playChoice = parseInt(
-    prompt("1)Rock\n2)Paper\n3)Scissors\nEnter your choice: ")
-  );
-  if (playChoice < 0 || playChoice > 3) {
-    alert("\nInvalid entry, try again.\n");
-    console.log("\nInvalid entry, try again.\n");
-    getSelection();
-  } else {
-    return checkChoice(playChoice - 1);
-  }
 }
 
 function checkChoice(num) {
@@ -29,37 +22,61 @@ function checkChoice(num) {
 function playRound(play, comp) {
   if (play == "rock") {
     if (comp == "paper") {
+      cs = cs + 1;
       return "You lose.  Paper covers rock.";
     } else if (comp == "scissors") {
+      ps = ps + 1;
       return "You win!  Rock smashes scissors.";
     } else {
       return "It's a draw!";
     }
   } else if (play == "paper") {
     if (comp == "rock") {
+      ps = ps + 1;
       return "You win!  Paper covers rock.";
     } else if (comp == "scissors") {
+      cs = cs + 1;
       return "You lose.  Scissors cut paper.";
     } else {
       return "It's a draw!";
     }
   } else {
     if (comp == "rock") {
+      cs = cs + 1;
       return "You lose.  Rock smashes scissors.";
     } else if (comp == "paper") {
+      ps = ps + 1;
       return "You win.  Scissors cut paper.";
     } else {
       return "It's a draw!";
     }
   }
 }
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = getSelection();
-    let computerSelection = computerPlay();
-    alert(playRound(playerSelection, computerSelection));
-    console.log(playRound(playerSelection, computerSelection));
+
+function game(choice) {
+  let message = "";
+  let computerSelection = computerPlay();
+  
+  message = `You chose ${choice}.\n
+  The computer chose ${computerSelection}.\n
+  ${playRound(choice, computerSelection)}`;
+
+  if (cs > 4 || ps > 4) {
+    message = "Game Over";
+    Array.from(document.querySelectorAll(".choice")).forEach((button) => {
+      button.className += "hidden";
+      button.textContent = "";
+    });
   }
+  result.textContent = message;
+  playerScore.textContent = ps;
+  computerScore.textContent = cs;
 }
 
-game();
+document.getElementById("results").appendChild(result);
+document.getElementById("player-score").appendChild(playerScore);
+document.getElementById("computer-score").appendChild(computerScore);
+
+Array.from(document.querySelectorAll(".choice")).forEach((button) => {
+  button.addEventListener("click", (choice) => game(choice.target.id));
+});
